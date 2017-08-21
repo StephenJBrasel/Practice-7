497920813633badb76ce957c623a005d03d72524
Stephen Brasel <stephen.brasel@gmail.com>
2017-08-21 14:43:46 -0400

    Added second Practice.

---
+++ P 7.02/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_7._02
+{
+    public class BankAccount
+    {
+        public string mCustomer;
+        float mBalance;
+        //constructors
+        public BankAccount() {}
+        public BankAccount(string name)
+        {
+            mCustomer = name;
+        }
+        public BankAccount(string name, float InitBalance)
+        {
+            mCustomer = name;
+            mBalance = InitBalance;
+        }
+        //methods
+        public void SetCustName (string name)
+        {
+            mCustomer = name;
+        }
+        public string GetCustomer ()
+        {
+            return mCustomer;
+        }
+        public float GetBalance()
+        {
+            return mBalance;
+        }
+        bool Withdraw (float ammount)
+        {
+            if (ammount >= 0)
+            {
+
+            }
+            return false;
+        }
+        bool Deposit (float ammount)
+        {
+            if (ammount >= 0)
+            {
+
+            }
+            return false;
+        }
+    }
+}
--- Practice 6/Program.cs
+++ Practice 6/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Practice_6
 {
+    class MySecondClass
+    {
+        public MySecondClass()
+        {
+            Console.WriteLine("Constructor!");
+        }
+        ~MySecondClass()
+        {
+            Console.WriteLine("Destructor!");
+            Console.ReadLine();
+        }
+        
+    }
+
     class Program
     {
         static void Main(string[] args)
-        {
+        {do
+            {
+                MySecondClass classy = new MySecondClass();
+                classy = null;
+                GC.Collect();
+
+                Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                Console.Write("Press ENTER to continue... ");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+
         }
     }
 }
